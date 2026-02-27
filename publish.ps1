125b77862127622c3c303c67450d498e630f830b
charlie-sans <gamerboy030607@gmail.com>
2026-02-28 02:27:43 +1100

    Remove obsolete solution file and update publish script to specify project path

--- publish.ps1
+++ publish.ps1
@@ -1,5 +1,5 @@
 $rids = "win-x64","linux-x64","linux-arm64","osx-x64","osx-arm64"
 foreach($rid in $rids) {
-  dotnet publish -c Release -r $rid --self-contained true /p:PublishSingleFile=true /p:EnableCompressionInSingleFile=true -o "publish/$rid"
+  dotnet publish Lattice.csproj -c Release -r $rid --self-contained true /p:PublishSingleFile=true /p:EnableCompressionInSingleFile=true -o "publish/$rid"
 }
 foreach ($item in Get-ChildItem publish) { zip $item.name "publish/$($item.name)" -r }
